1d72d2af8630bf105147c73a2d3dd94ba4f9b651
Anant <anant.mathur007@gmail.com>
2020-09-06 12:34:32 +0530

    project finished

---
+++ README.md
@@ -0,0 +1,22 @@
+## SIMPLE QUIZ APP USING REACT AND TYPESCRIPT (Using Open Trivia API)
+
+### Project Setup
+
+```javascript
+1. Clone the repo
+2. cd typescript-react-quiz-app
+3. npm install
+4. npm start
+5. The app should open on localhost: 3000
+```
+
+### Tech Stack:
+
+- React.js
+- Typescript
+- Styled Components
+
+### Credits:
+
+- FreeCodeCamp (https://www.youtube.com/watch?v=F2JCjVSZlG0)
+- Weibenfalk (https://www.youtube.com/user/Weibenfalk)
---
+++ src/App.tsx
@@ -0,0 +1,114 @@
+import React, { useState, Fragment } from "react";
+import { fetchQuizQuestions } from "./API";
+// Components
+import QuestionCard from "./components/QuestionCard";
+// Types
+import { QuestionState, Difficulty } from "./API";
+// Styles
+import { GlobalStyle, Wrapper } from "./App.styles";
+
+export interface AnswerObject {
+  question: string;
+  answer: string;
+  correct: boolean;
+  correctAnswer: string;
+}
+const TOTAL_QUESTIONS = 10;
+
+const App = () => {
+  const [loading, setLoading] = useState(false);
+  const [questions, setQuestions] = useState<QuestionState[]>([]);
+  const [number, setNumber] = useState(0);
+  const [userAnswers, setUserAnswers] = useState<AnswerObject[]>([]);
+  const [score, setScore] = useState(0);
+  const [gameOver, setGameOver] = useState(true);
+
+  const startTrivia = async () => {
+    setLoading(true);
+    setGameOver(false);
+
+    const newQuestions = await fetchQuizQuestions(
+      TOTAL_QUESTIONS,
+      Difficulty.EASY
+    );
+
+    setQuestions(newQuestions);
+    setScore(0);
+    setUserAnswers([]);
+    setNumber(0);
+
+    setLoading(false);
+  };
+
+  const checkAnswer = (e: React.MouseEvent<HTMLButtonElement>) => {
+    if (!gameOver) {
+      // Users answer
+      const answer = e.currentTarget.value;
+
+      // Check answer against correct value
+      const correct = questions[number].correct_answer === answer;
+
+      // Add score if answer is correct
+      if (correct) {
+        setScore(score + 1);
+      }
+
+      // Save answer in array for User answers
+      const answerObject = {
+        question: questions[number].question,
+        answer,
+        correct,
+        correctAnswer: questions[number].correct_answer,
+      };
+      setUserAnswers((prev) => [...prev, answerObject]);
+    }
+  };
+
+  const nextQuestion = () => {
+    // Move on to next question if not the lst question
+    const nextQuestion = number + 1;
+
+    if (nextQuestion === TOTAL_QUESTIONS) {
+      setGameOver(true);
+    } else {
+      setNumber(nextQuestion);
+    }
+  };
+
+  return (
+    <Fragment>
+      <GlobalStyle />
+      <Wrapper>
+        <h1>REACT QUIZ</h1>
+        {gameOver || userAnswers.length === TOTAL_QUESTIONS ? (
+          <button className="start" onClick={startTrivia}>
+            Start
+          </button>
+        ) : null}
+
+        {!gameOver ? <p className="score">Score: {score}</p> : null}
+        {loading && <p>Loading Questions...</p>}
+        {!loading && !gameOver && (
+          <QuestionCard
+            questionNr={number + 1}
+            totalQuesitons={TOTAL_QUESTIONS}
+            question={questions[number].question}
+            answers={questions[number].answers}
+            userAnswer={userAnswers ? userAnswers[number] : undefined}
+            callback={checkAnswer}
+          />
+        )}
+        {!loading &&
+        !gameOver &&
+        userAnswers.length === number + 1 &&
+        number !== TOTAL_QUESTIONS - 1 ? (
+          <button className="next" onClick={nextQuestion}>
+            Next Question
+          </button>
+        ) : null}
+      </Wrapper>
+    </Fragment>
+  );
+};
+
+export default App;
