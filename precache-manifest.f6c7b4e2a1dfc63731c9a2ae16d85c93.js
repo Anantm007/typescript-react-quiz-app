3b5b8a48d51c2ff05ff2721d9ab03808aa7583fd
Anant <anant.mathur007@gmail.com>
2021-02-01 18:23:15 +0530

    Updates

---
+++ precache-manifest.f6c7b4e2a1dfc63731c9a2ae16d85c93.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51c20ebc82fccb03c57cb417beab99f2",
+    "url": "/typescript-react-quiz-app/index.html"
+  },
+  {
+    "revision": "e1044775fd5890ded7c7",
+    "url": "/typescript-react-quiz-app/static/js/2.68a92814.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/typescript-react-quiz-app/static/js/2.68a92814.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "46b4901e515b611a242f",
+    "url": "/typescript-react-quiz-app/static/js/main.240097a6.chunk.js"
+  },
+  {
+    "revision": "87e906ec930e4c5093d0",
+    "url": "/typescript-react-quiz-app/static/js/runtime-main.85143aae.js"
+  },
+  {
+    "revision": "c11f97a3b321dad2ede69652d3f8a174",
+    "url": "/typescript-react-quiz-app/static/media/nattu-adnan-unsplash.c11f97a3.jpg"
+  }
+]);
